15e5954b6d95dc0a1c3191bcce48b588143e5a4a
Chriztech <ajinjoy1997@gmail.com>
2023-08-22 13:35:33 +0530

    TYPECASTING

---
+++ src/variables/TypeCasting.java
@@ -0,0 +1,16 @@
+package variables;
+
+public class TypeCasting {
+	public static void main(String[] args) {
+		byte t=7;
+	      //type casting
+	      int y=(int)t;
+	      //implicit conversion
+	      float f=y;
+	      System.out.println(y);
+	      System.out.println(f);
+	      
+		
+	}
+     
+}
