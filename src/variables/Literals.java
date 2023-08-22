bcf1397d01d8e28d2efedfaf0cc9685c8910defa
Chriztech <ajinjoy1997@gmail.com>
2023-08-22 12:47:27 +0530

    literals

---
+++ src/variables/Literals.java
@@ -0,0 +1,10 @@
+package variables;
+
+public class Literals {
+	public static void main(String Args[]) {
+	//here 90 is called literal
+	int a=90;
+	int y=100_00_00_00;
+	System.out.println(y);
+}
+}
